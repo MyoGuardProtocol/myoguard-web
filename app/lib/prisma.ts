60c7d3be2a2b19338822dd30aaffc627bb8c74fe
MyoGuard Protocol <myoguardprotocol@gmail.com>
2026-03-14 12:59:41 +0000

    Pass DATABASE_URL to PrismaClient

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const globalForPrisma = globalThis as any
 
 if (!globalForPrisma.prisma) {
   const { PrismaClient } = require("@prisma/client")
-  globalForPrisma.prisma = new PrismaClient()
+  globalForPrisma.prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 }
 
 export const prisma = globalForPrisma.prisma
