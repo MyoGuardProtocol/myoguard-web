95885272d94a7b5f35fa7edbf3ce87a761e18110
MyoGuard Protocol <myoguardprotocol@gmail.com>
2026-03-14 01:14:51 +0000

    Move ClerkProvider to client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "./providers";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,12 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <Providers>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    </Providers>
   );
 }
