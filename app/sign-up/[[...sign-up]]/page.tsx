b6a77e481d50ef7b7a097432477a3d1e99d0f3f5
MyoGuard Protocol <myoguardprotocol@gmail.com>
2026-03-14 00:20:29 +0000

    Add heading to sign-up page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,9 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs"
 
 export default function SignUpPage() {
   return (
-    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f9fafb' }}>
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "#f9fafb" }}>
+      <h1 style={{ color: "black", marginBottom: "20px" }}>Sign Up for MyoGuard</h1>
       <SignUp />
     </div>
   )
